77bee309bd305658ed42f094da22315e32d03f26
leumij <cjimuel74@gmail.com>
2024-01-26 13:32:31 +0800

    for range

--- printthesum.py
+++ printthesum.py
@@ -2,4 +2,12 @@
 
 # pseudocode
 
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
+
 # loop 1 to 10
+
+for i in range(1, 11):
+    sum_range = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", sum_range)
+    previous_num = i
